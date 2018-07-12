716abf315327dcf15396cc4111e44b078b7c8d6c
PereiraMarcos5 <38700005+PereiraMarcos5@users.noreply.github.com>
2018-07-12 17:17:36 -0300

    Começado

---
+++ SistemaExercicioSqlCSharp01/SistemaExercicioSqlCSharp01/Model/Alunos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaExercicioSqlCSharp01.Model
+{
+    class Alunos
+    {
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public double Nota1 { get; set; }
+        public double Nota2 { get; set; }
+        public double Nota3 { get; set; }
+        public int frequencia { get; set; }
+
+    }
+}
